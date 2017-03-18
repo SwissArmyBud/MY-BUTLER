174cb09fa39eb1a29e75634217a8ffcb6d3cb580
APorterCSUMB <APorter@CSUMB.edu>
2017-03-18 14:39:27 -0700

    Fixed issues in MQTT install script

--- Software/installMQTT.sh
+++ Software/installMQTT.sh
@@ -1,5 +1,5 @@
-!# /bin/bash
+#! /bin/bash
 
-sudo apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
+sudo apt-add-repository ppa:mosquitto-dev/mosquitto-ppa -y
 sudo apt-get update
-sudo apt-get install mosquitto libmosquitto-dev mosquitto-clients
+sudo apt-get install mosquitto libmosquitto-dev mosquitto-clients -y
