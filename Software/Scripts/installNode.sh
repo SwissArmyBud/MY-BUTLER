21247a94d03962562eab1425025786a1947754ec
Andrew Porter <APorter@CSUMB.edu>
2017-03-18 19:46:02 -0700

    Fine tuning Cloud Install Scripts

--- Software/Scripts/installNode.sh
+++ Software/Scripts/installNode.sh
@@ -2,15 +2,21 @@
 
 echo ""
 echo ""
-echo " --> CONFIGURING NODE INSTALL OPTIONS"
+echo " --> CONFIGURING NODEJS INSTALL VERSION"
 echo ""
 sudo curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 echo ""
 
 echo ""
 echo ""
-echo " --> INSTALLING NODEJS"
+echo " --> INSTALLING NPM"
 echo ""
 sudo apt-get install npm -y
+echo ""
+
+echo ""
+echo ""
+echo " --> INSTALLING NODE"
+echo ""
 sudo apt-get install nodejs -y
 echo ""
--- Software/Scripts/installOHCloud.sh
+++ Software/Scripts/installOHCloud.sh
@@ -2,7 +2,7 @@
 
 echo ""
 echo ""
-echo " --> INSTALLING OPENHAB CLOUD"
+echo " --> DOWNLOADING OPENHAB CLOUD"
 echo ""
 cd ~
 sudo git clone https://github.com/openhab/openhab-cloud.git OHCloud
