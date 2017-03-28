f484f1dd922a3f6c9383c1c7bd6711c75fe6f817
Andrew Porter <APorter@CSUMB.edu>
2017-03-28 12:16:57 -0700

    Fancy home directory, output commenting tweak

--- Software/Tools/mongodump.sh
+++ Software/Tools/mongodump.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 
-cd ~/OHCloud
+# GO HOME
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+cd $USER_HOME
+
+# GO TO OHCloud INSTALLATION
+cd ./OHCloud
 
 mongodump
 
@@ -11,5 +16,5 @@ done
 cat ./dump/openhab/emailVerifications.bson.json
 
 echo ""
-echo "To activate, follow link format below and submit from browser:"
+echo "To activate, follow link format below and submit from HTML browser:"
 echo "http<s>://<IP/DNS>/verify?code=<CODE>"
