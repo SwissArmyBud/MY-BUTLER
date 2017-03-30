596da83bead79ff28edb95630866c637371ac9d5
Ubuntu <ubuntu@ip-172-31-14-122.us-west-1.compute.internal>
2017-03-30 01:13:23 +0000

    Verified MongoDummp Script

--- Software/Tools/mongodump.sh
+++ Software/Tools/mongodump.sh
@@ -5,16 +5,18 @@ USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 cd $USER_HOME
 
 # GO TO OHCloud INSTALLATION
-cd ./OHCloud
+cd $USER_HOME/OHCloud
 
 mongodump
 
-for f in ./dump/openhab/*bson
+for f in $USER_HOME/OHCloud/dump/openhab/*bson
 	do bsondump "$f" > "$f.json"
 done
 
-cat ./dump/openhab/emailVerifications.bson.json
+cat $USER_HOME/OHCloud/dump/openhab/emailverifications.bson.json
 
 echo ""
 echo "To activate, follow link format below and submit from HTML browser:"
-echo "http<s>://<IP/DNS>/verify?code=<CODE>"
+echo "  http<s>://<IP/DNS>/verify?code=<CODE>"
+echo ""
+echo ""
