b2e125a495190a5221f1a1dae5d5a0c9cec6043f
Andrew Porter <APorter@CSUMB.edu>
2017-04-01 23:50:55 -0700

    Added service script/installer for OH2

---
+++ Software/Tools/enableOH2.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# GO HOME
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+cd $USER_HOME
+
+
+# INSTALLING OH2 SERVICE
+echo ""
+echo ""
+echo "  --> *INSTALLING SERVICE FILE*"
+echo ""
+sudo cp $USER_HOME/MYBUTLER/Software/Configuration/openhab.service /lib/systemd/system/openhab2.service
+echo ""
+
+# CONFIGURING LINUX TO START OH2
+echo ""
+echo ""
+echo "  ---> *ENABLING OpenHAB 2 AS SERVICE*"
+echo ""
+sudo systemctl daemon-reload
+sudo systemctl enable openhab2.service
+echo ""
+
+# CONFIGURING PM2 TO START AT RUNTIME
+echo ""
+echo ""
+echo "  --> *STARTING AND CHECKING SERVICE*"
+echo ""
+sudo systemctl start openhab2.service
+sudo systemctl status openhab2.service
+echo ""
